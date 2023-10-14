1ce9910cbfe98ccd32a444ce5a6b9d813de8bfb7
Paul Pauls <mail@paulpauls.de>
2023-10-14 20:50:47 +0200

    Adapt example to the example in README

--- tests/example.py
+++ tests/example.py
@@ -1,11 +1,12 @@
 import cyclic_toposort as ct
 
-edges = {(0, 1), (1, 2), (2, 3), (3, 0)}
-result = ct.cyclic_toposort(edges, start_node=1)
-
+cyclic_graph_edges = {(1, 2), (2, 3), (3, 5), (3, 6), (4, 1), (4, 5), (4, 6), (5, 2), (5, 7), (6, 1), (8, 6)}
+result = ct.cyclic_toposort(cyclic_graph_edges)
 print(result)
 
-edges = {(1, 2), (2, 3), (3, 5), (3, 6), (4, 1), (4, 5), (4, 6), (5, 2), (5, 7), (6, 1), (8, 6)}
-result = ct.cyclic_toposort(edges, start_node=1)
+result = ct.cyclic_toposort(cyclic_graph_edges, start_node=2)
+print(result)
 
+acyclic_graph_edges = {(1, 2), (1, 3), (2, 3), (2, 4), (3, 4), (5, 3), (5, 6), (7, 6)}
+result = ct.acyclic_toposort(acyclic_graph_edges)
 print(result)
