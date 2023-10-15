d521143e05b903a61c15e2dc841992e846e814de
Paul Pauls <mail@paulpauls.de>
2023-10-15 23:13:47 +0200

    Remove debug prints and adapt to pre-commit checks

--- tests/cyclic_toposort_test.py
+++ tests/cyclic_toposort_test.py
@@ -5,12 +5,13 @@ from pathlib import Path
 import yaml
 from graphviz import Digraph
 
-from cyclic_toposort import cyclic_toposort
+from cyclic_toposort.cyclic_toposort import cyclic_toposort
 from tests.test_utils import bruteforce_toposort, create_random_graph
 
 TEST_GRAPHS_DIR = "./test_graphs/"
 TEST_RESULTS_YAML = "./test_results.yaml"
 CYCLIC_NODES_PROBABILITY = 0.2
+START_NODE_PROBABILITY = 0.2
 
 
 def test_random_graphs_against_bruteforce() -> None:
@@ -22,31 +23,29 @@ def test_random_graphs_against_bruteforce() -> None:
     test_results_yaml = Path(TEST_RESULTS_YAML)
     test_results_yaml.resolve()
 
-    test_results: dict[str, dict] = {}
+    test_results: dict[str, dict] = {}  # type: ignore[type-arg]
     for i in range(100):
         test_name = f"test_graph_{i}"
 
         num_edges = random.randint(8, 16)
         cyclic_nodes = random.random() < CYCLIC_NODES_PROBABILITY
         edges = create_random_graph(num_edges=num_edges, cyclic_nodes=cyclic_nodes)
+        start_node = None
+        if random.random() < START_NODE_PROBABILITY:
+            start_node = random.choice(list(edges))[0]
 
         graph = Digraph(graph_attr={"rankdir": "TB"})
         for edge_start, edge_end in edges:
             graph.edge(str(edge_start), str(edge_end))
         graph.render(filename=test_name, directory=test_graphs_dir, view=False, cleanup=True, format="svg")
 
-        print(i)
-
         algorithm_results = cyclic_toposort(edges=edges)
-        print("+")
-
         bruteforce_results = bruteforce_toposort(edges=edges)
-        print("-")
-
         assert algorithm_results in bruteforce_results
 
         test_results[test_name] = {
             "edges": edges,
+            "start_node": start_node,
             "algorithm_results": algorithm_results,
             "bruteforce_results": bruteforce_results,
         }
